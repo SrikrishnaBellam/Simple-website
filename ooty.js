729cb9019a7d719f13e894d3a0c63befe97e3427
SriKrishna Bellam <bellamsrikrishna@gmail.com>
2023-01-23 16:01:34 +0530

    first and last commit

---
+++ ooty.js
@@ -0,0 +1,15 @@
+var queryURL = "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys/";
+
+$.getJSON(queryURL, function (data) {
+  
+  var results = data.query.results
+  var firstResult = results.channel.item.condition
+  console.log(firstResult);
+  
+  var location = 'Chennai' // not returned in response
+  var temp = firstResult.temp
+  var text = firstResult.text
+  
+  $('#output').append('The temperature is ' + temp + '. Forecast calls for '+text);
+  
+})
